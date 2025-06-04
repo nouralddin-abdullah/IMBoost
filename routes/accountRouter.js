f09d14fbfee6cd6524ce587323c6edc4f949c754
nightstalker5699 <94228182+nightstalker5699@users.noreply.github.com>
2025-06-05 00:47:02 +0300

    tt

--- routes/accountRouter.js
+++ routes/accountRouter.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const accountController = require("../controllers/accountController");
 const authController = require("../controllers/authController");
-router.use(authController.restrictTo("admin"));
 
 router.post("/", accountController.addAccount);
 router.delete("/:id", accountController.deleteAccount);
+router.use(authController.restrictTo("admin"));
 router.put("/:id", accountController.updateAccount);
 router.post("/refresh/:id", accountController.refreshLogin);
 router.post("/refresh-all", accountController.refreshAllAccounts);
