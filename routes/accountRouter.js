b2a2ea07299a53926b1fb6610a30dab21bce4619
Nouralddin <43034992+nouralddin-abdullah@users.noreply.github.com>
2025-06-05 00:51:40 +0300

    Update accountRouter.js

--- routes/accountRouter.js
+++ routes/accountRouter.js
@@ -2,17 +2,11 @@ const express = require("express");
 const router = express.Router();
 const accountController = require("../controllers/accountController");
 const authController = require("../controllers/authController");
-<<<<<<< HEAD
 
-// Apply authentication and admin restriction to all routes
 router.use(authController.protect);
 router.use(authController.restrictTo("admin"));
-=======
->>>>>>> f09d14fbfee6cd6524ce587323c6edc4f949c754
-
 router.post("/", accountController.addAccount);
 router.delete("/:id", accountController.deleteAccount);
-router.use(authController.restrictTo("admin"));
 router.put("/:id", accountController.updateAccount);
 router.post("/refresh/:id", accountController.refreshLogin);
 router.post("/refresh-all", accountController.refreshAllAccounts);
