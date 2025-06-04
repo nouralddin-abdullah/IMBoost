5b8276346c693ff634119272f8126349d077e50c
nightstalker5699 <94228182+nightstalker5699@users.noreply.github.com>
2025-06-05 00:34:40 +0300

    restricted

--- routes/accountRouter.js
+++ routes/accountRouter.js
@@ -1,12 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const accountController = require('../controllers/accountController');
+const accountController = require("../controllers/accountController");
+const authController = require("../controllers/authController");
+router.use(authController.restrictTo("admin"));
 
-router.post('/', accountController.addAccount);
-router.put('/:id', accountController.updateAccount);
-router.delete('/:id', accountController.deleteAccount);
-router.post('/refresh/:id', accountController.refreshLogin); 
-router.post('/refresh-all', accountController.refreshAllAccounts);
-router.post('/change-images', accountController.changeAccountImages);
+router.post("/", accountController.addAccount);
+router.delete("/:id", accountController.deleteAccount);
+router.put("/:id", accountController.updateAccount);
+router.post("/refresh/:id", accountController.refreshLogin);
+router.post("/refresh-all", accountController.refreshAllAccounts);
+router.post("/change-images", accountController.changeAccountImages);
 
 module.exports = router;
